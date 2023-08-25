c28c6b51bc481776697ddfb27783766c3c216aa0
Vitor <111993162+ViiTii@users.noreply.github.com>
2023-08-25 16:35:26 -0300

    menu mobile

--- estudo/animais-fantasticos/js/LoginModal.js
+++ estudo/animais-fantasticos/js/LoginModal.js
@@ -1,18 +1,30 @@
-const AbrirLogin = document.querySelector('button')
-const LoginModal = document.querySelector('dialog')
-const CloseLogin = document.getElementById('CloseLogin')
-const dialog = document.getElementById('my-dialog');
+// const AbrirLogin = document.getElementById('AbrirLogin')
+// const LoginModal = document.querySelector('dialog')
+// const CloseLogin = document.getElementById('CloseLogin')
 
-AbrirLogin.onclick = function () {
-  LoginModal.showModal()
+// AbrirLogin.onclick = function () {
+//   LoginModal.showModal()
+// }
+
+// CloseLogin.onclick = function () {
+//   LoginModal.close()
+// }
+
+// const dialog = document.querySelector('dialog:modal');
+
+// dialog.addEventListener('click', (clickOutside) => {
+//   if (clickOutside.target !== clickOutside.currentTarget) {
+//     dialog.close();
+//   }
+// })
+
+function change() {
+  const get = document.querySelector(".menu-options")
+  get.classList.toggle("active")
 }
 
-CloseLogin.onclick = function () {
-  LoginModal.close()
-}
+document.getElementById('MenuButton').addEventListener('click', change)
 
-dialog.addEventListener('click', (clickOutside) => {
-  if (clickOutside.target.id !== 'modal') {
-    dialog.close();
-  }
-});
+
+
+ 
